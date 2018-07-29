9de9390d4b7523a22623b2d382e8e1d5d6a9aeab
mitsuntu <t.mitsui000000@gmail.com>
2018-07-29 22:44:20 +0900

    refs #180729 add base codes

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -15,6 +15,14 @@ try {
 } catch (e) {}
 
 /**
+ * Vue is a modern JavaScript library for building interactive web interfaces
+ * using reactive data binding and reusable components. Vue's API is clean
+ * and simple, leaving you to focus on building your next great project.
+ */
+
+window.Vue = require('vue');
+
+/**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
@@ -22,7 +30,10 @@ try {
 
 window.axios = require('axios');
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common = {
+    'X-CSRF-TOKEN': window.Laravel.csrfToken,
+    'X-Requested-With': 'XMLHttpRequest'
+};
 
 /**
  * Next we will register the CSRF Token as a common header with Axios so that
--- task.md
+++ task.md
@@ -2,6 +2,9 @@
 - How to implement on Laravel?
 -  
 
+#Specifications
+
+
 #Refferance
 Setting of VSCode
 - https://qiita.com/mnao305/items/124b1525a8561eec37f4
@@ -9,3 +12,19 @@ Sample implementation
 - https://luftgarden.work/laravel-vue-attendance-part1/
 How to set Controller on Laravel
 - https://qiita.com/yu_eguchi/items/d6ac0fa3e0014db536c3
+
+
+
+#How to implement 
+-js
+resources/assets/js/app.js
+
+- .vue
+resources/assets/js/components/Example.vue
+
+- Routing
+routes/web.php
+
+- Views from Routing
+resources/views/index.blade.php
+
